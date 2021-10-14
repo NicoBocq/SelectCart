bf495457d4727f2b8303459aabf6dbb14ea2f1e4
NicoBocq <nicolas.fow@gmail.com>
2021-10-14 14:56:25 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,40 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      transparent: "transparent",
+      transparent: 'transparent',
+      white: colors.white,
       gray: colors.blueGray,
       blue: colors.sky,
       yellow: colors.yellow,
+      red: colors.red,
       brand: {
-        50: "#FFFBEB",
-        100: "#FEF3C7",
-        200: "#FDE68A",
-        300: "#FCD34D",
-        400: "#FBBF24",
-        500: "#F59E0B",
-        600: "#D97706",
-        700: "#B45309",
-        800: "#92400E",
-        900: "#78350F",
-      },
+        50: '#FFFBEB',
+        100: '#FEF3C7',
+        200: '#FDE68A',
+        300: '#FCD34D',
+        400: '#FBBF24',
+        500: '#F59E0B',
+        600: '#D97706',
+        700: '#B45309',
+        800: '#92400E',
+        900: '#78350F'
+      }
     },
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {
     extend: {
-      rotate: ["group-hover"],
-    },
+      rotate: ['group-hover']
+    }
   },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [require('@tailwindcss/forms')]
+}
