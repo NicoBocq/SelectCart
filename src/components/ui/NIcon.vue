a5fea8ffd83e181c324d88b5cbaedee5fcad5c7d
NicoBocq <nicolas.fow@gmail.com>
2021-10-14 14:56:54 +0200

    init components

---
+++ src/components/ui/NIcon.vue
@@ -0,0 +1,55 @@
+<template>
+  <component
+    :is="processedIcon"
+    :class="sizeStyle"
+    class="inline-block"
+    aria-hidden="true"
+  />
+</template>
+
+<script>
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  StarIcon
+} from '@heroicons/vue/solid'
+
+import { computed, toRefs } from 'vue'
+export default {
+  name: 'NIcon',
+  components: {
+    CheckIcon,
+    ChevronDownIcon,
+    StarIcon
+  },
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    small: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props) {
+    const { icon, small, size } = toRefs(props)
+    const processedIcon = computed(() => {
+      return `${icon.value}-icon`
+    })
+
+    const sizeStyle = computed(() => {
+      return small.value ? 'h-4 w-4' : size.value ? size.value : 'h-5 w-5'
+    })
+
+    return {
+      processedIcon,
+      sizeStyle
+    }
+  }
+}
+</script>
